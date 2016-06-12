1490c48237a25a0622db28921f42025fe1c04f42
Jorgesanel <jorgesantosneila@hotmail.com>
2016-06-12 19:15:58 +0200

    Done iterative header

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,14 @@ $(function(){
 	$("#header").load("header.html"); 
 	$("#footer").load("footer.html"); 
 });
+
+$(function() {
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if (scroll >= 100) {
+			$(".header").addClass('smaller');
+		} else {
+			$(".header").removeClass("smaller");
+		}
+	});
+});
